4a591cce6edeed7bfb4be7c6af0a5211bb046444
Tetiana <tatiana.makarenko94@gmail.com>
2025-06-29 20:29:44 +0300

    LoadMoreBtn

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,20 +1,13 @@
-const LoadMoreBtn = () => {
-  return <div>LoadMoreBtn</div>;
+import css from './LoadMoreBtn.module.css';
+
+const LoadMoreBtn = ({ onClick }) => {
+  return (
+    <div className={css.wrapper}>
+      <button className={css.button} onClick={onClick}>
+        Load more
+      </button>
+    </div>
+  );
 };
 
 export default LoadMoreBtn;
-
-//---------------added Haievska-------------------
-// import css from './LoadMoreBtn.module.css';
-
-// const LoadMoreBtn = ({ onClick }) => {
-//   return (
-//     <div className={css.wrapper}>
-//       <button className={css.button} onClick={onClick}>
-//         Load more
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default LoadMoreBtn;
