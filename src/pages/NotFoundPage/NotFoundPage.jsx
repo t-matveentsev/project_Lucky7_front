4c69038365c2b39e9f4f43750278d3e4c0930860
Мосендз Юлія Андріївна <mosendzjulia@MacBook-Pro-Mosendz.local>
2025-07-01 16:25:17 +0300

    add back to main btn

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import Container from '../../components/Container/Container.jsx';
 import css from './NotFoundPage.module.css';
+import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
   const [pageHeight, setPageHeight] = useState(0);
@@ -22,6 +23,7 @@ const NotFoundPage = () => {
         }}
         className={css.pageWrapper}
       >
+        <Link className={css.btnBack} to="/">Back to Main</Link>
         <h2>This recipe page is not found</h2>
       </div>
     </Container>
