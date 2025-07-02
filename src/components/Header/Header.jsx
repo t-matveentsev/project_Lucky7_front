4d37b74083d492deb85c501ff9a7393c210dc277
Imojenga <aina.jenko@gmail.com>
2025-07-02 17:09:58 +0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,29 @@
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
 import Navigation from '../Navigation/Navigation';
 import UserMenu from '../UserMenu/UserMenu';
 import AuthMenu from '../AuthMenu/AuthMenu';
 
 const Header = () => {
-  const isLoggedIn = false;
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
   return (
     <header>
       <nav>
         <Navigation />
-        {isLoggedIn ? <UserMenu /> : <AuthMenu />}
+        <div>{isLoggedIn ? <UserMenu /> : <AuthMenu />}</div>
+        <button>
+          <svg>
+            <use></use>
+          </svg>
+        </button>
+        <button>
+          <svg>
+            <use></use>
+          </svg>
+        </button>
       </nav>
+      <div>{isLoggedIn ? <UserMenu /> : <AuthMenu />}</div>
     </header>
   );
 };
