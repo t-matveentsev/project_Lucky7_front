b02e1dcd4f7bc84c8624daba80f6c85a2088b2d3
BoredDarkness <gwinnblaidd@gmail.com>
2025-07-05 01:05:31 +0200

    interceptor

--- src/redux/auth/operation.js
+++ src/redux/auth/operation.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { logoutThunk } from './operation';
 
 export const api = axios.create({
   baseURL: 'https://project-lucky7.onrender.com/api/',
+  withCredentials: true,
 });
 
 const setAuthHeader = token => {
@@ -10,15 +12,53 @@ const setAuthHeader = token => {
 };
 
 const clearAuthHeader = () => {
-  api.defaults.headers.common.Authorization = ``;
+  api.defaults.headers.common.Authorization;
 };
 
+export const applyInterceptor = store => {
+  api.interceptors.request.use(
+    config => {
+      const token = store.getState().auth.token;
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    error => Promise.reject(error)
+  );
+
+  api.interceptors.response.use(
+    res => res,
+    async error => {
+      const originalRequest = error.config;
+
+      if (error.response?.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+
+        try {
+          const refreshResponse = await api.post('/auth/refresh');
+          const newToken = refreshResponse.data.data.accessToken;
+
+          store.dispatch({ type: 'auth/refresh/fulfilled', payload: newToken });
+
+          setAuthHeader(newToken);
+          originalRequest.headers.Authorization = `Bearer ${newToken}`;
+          return api(originalRequest);
+        } catch (err) {
+          store.dispatch(logoutThunk());
+          return Promise.reject(err);
+        }
+      }
+
+      return Promise.reject(error);
+    }
+  );
+};
 export const registerThunk = createAsyncThunk(
   'auth/register',
   async (body, thunkAPI) => {
     try {
       const { data } = await api.post('/auth/register', body);
-      setAuthHeader(data.token);
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -35,13 +75,50 @@ export const registerThunk = createAsyncThunk(
 //     return thunkAPI.rejectWithValue(error.message);
 //   }
 // });
+//export const loginThunk = createAsyncThunk(
+// 'auth/login',
+// async (body, thunkAPI) => {
+//  try {
+//   const { data } = await api.post('/auth/login', body);
+//   setAuthHeader(data.token);
+//   return data;
+//  } catch (error) {
+//    return thunkAPI.rejectWithValue(error.message);
+//   }
+//  }
+//);
 export const loginThunk = createAsyncThunk(
   'auth/login',
   async (body, thunkAPI) => {
     try {
       const { data } = await api.post('/auth/login', body);
-      setAuthHeader(data.token);
-      return data;
+      const accessToken = data.data.accessToken;
+      setAuthHeader(accessToken);
+      return data.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+export const logoutThunk = createAsyncThunk(
+  'auth/logout',
+  async (_, thunkAPI) => {
+    try {
+      await api.post('/auth/logout');
+      clearAuthHeader();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+export const refreshThunk = createAsyncThunk(
+  'auth/refresh',
+  async (_, thunkAPI) => {
+    try {
+      const { data } = await api.post('/auth/refresh');
+      const accessToken = data.data.accessToken;
+      setAuthHeader(accessToken);
+      return accessToken;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
