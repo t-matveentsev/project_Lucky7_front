b853d7bf769d912f36109fceaf76fe184a49e151
Tetiana <tatiana.makarenko94@gmail.com>
2025-07-04 23:42:42 +0300

    add PrivateRoute, fixed ProfilePage.jsx

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,5 +1,20 @@
-const PrivateRoute = () => {
-  return <div>PrivateRoute</div>;
+// const PrivateRoute = () => {
+//   return <div>PrivateRoute</div>;
+// };
+
+// export default PrivateRoute;
+
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { selectIsLoggedIn, selectIsRefreshing } from '../redux/auth/selectors';
+
+const PrivateRoute = ({ children }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isRefreshing = useSelector(selectIsRefreshing);
+
+  if (isRefreshing) return null; // або Loader
+
+  return isLoggedIn ? children : <Navigate to="/auth/login" />;
 };
 
 export default PrivateRoute;
