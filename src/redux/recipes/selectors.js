8b2733be695a6c670c130eb460724110471f926a
Tetiana <tatiana.makarenko94@gmail.com>
2025-07-05 13:56:15 +0300

    fixed ProfilePage and ProfileNavigation

--- src/redux/recipes/selectors.js
+++ src/redux/recipes/selectors.js
@@ -2,3 +2,4 @@ export const selectRecipes = state => state.recipes.items;
 export const selectIsLoading = state => state.recipes.isLoading;
 export const selectHasMore = state => state.recipes.hasMore;
 export const selectTotalRecipes = state => state.recipes.total;
+export const selectFavorites = state => state.recipes.favorites;
