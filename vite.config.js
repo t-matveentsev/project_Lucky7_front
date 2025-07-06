3ad9831b907b7fa241b37c4246754ae2d427b918
Artem Matvieientsev <tmatveentsev@gmail.com>
2025-07-06 18:05:03 +0200

    vite config test

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,26 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://project-lucky7.onrender.com', // http://localhost:3000
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://project-lucky7.onrender.com', // http://localhost:3000
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+  build: {
+    sourcemap: true,
   },
 });
