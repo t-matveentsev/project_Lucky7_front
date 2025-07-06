8d619de020a6734eed5325749953091a8d8a0a73
Artem Matvieientsev <tmatveentsev@gmail.com>
2025-07-06 17:40:01 +0200

    style for MainPage, adaptive

--- src/components/SearchRecipes/SearchRecipes.jsx
+++ src/components/SearchRecipes/SearchRecipes.jsx
@@ -6,50 +6,56 @@ const SearchRecipes = ({ onSearch }) => {
   return (
     <div>
       <div className={css.hero}>
-        <h1 className={css.heroHeader}>Plan, Cook, and Share Your Flavors</h1>
-        <Formik
-          initialValues={{ search: '' }}
-          validationSchema={validationSchema}
-          validateOnChange={false}
-          validateOnBlur={false}
-          onSubmit={(values, { setSubmitting, resetForm }) => {
-            const cleanedQuery = values.search.trim().toLowerCase().replace(/[\s-_]/g, '');
-            onSearch(cleanedQuery);
-            setSubmitting(false);
-            resetForm();
-          }}
-        >
-          {({ handleSubmit }) => (
-            <Form
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  e.preventDefault();
-                  handleSubmit();
-                }
+        <div className={css.searchWrapper}>
+          <h1 className={css.heroHeader}>Plan, Cook, and Share Your Flavors</h1>
+          <div className={css.formikStyle}>
+            <Formik
+              initialValues={{ search: '' }}
+              validationSchema={validationSchema}
+              validateOnChange={false}
+              validateOnBlur={false}
+              onSubmit={(values, { setSubmitting, resetForm }) => {
+                const cleanedQuery = values.search
+                  .trim()
+                  .toLowerCase()
+                  .replace(/[\s-_]/g, '');
+                onSearch(cleanedQuery);
+                setSubmitting(false);
+                resetForm();
               }}
             >
-              <div className={css.searchForm}>
-              <Field
-              as="input"
-                type="text"
-                name="search"
-                id="search"
-                className={css.input}
-                placeholder="Search recipes"
-                />
-              <button type="submit" className={css.button}>
-                Search
-              </button>                
-                </div>
-              <ErrorMessage
-                name="search"
-                component="div"
-                className={css.error}
-              />
-
-            </Form>
-          )}
-        </Formik>
+              {({ handleSubmit }) => (
+                <Form
+                  onKeyDown={e => {
+                    if (e.key === 'Enter') {
+                      e.preventDefault();
+                      handleSubmit();
+                    }
+                  }}
+                >
+                  <div className={css.searchForm}>
+                    <Field
+                      as="input"
+                      type="text"
+                      name="search"
+                      id="search"
+                      className={css.input}
+                      placeholder="Search recipes"
+                    />
+                    <button type="submit" className={css.button}>
+                      Search
+                    </button>
+                  </div>
+                  <ErrorMessage
+                    name="search"
+                    component="div"
+                    className={css.error}
+                  />
+                </Form>
+              )}
+            </Formik>
+          </div>
+        </div>
       </div>
     </div>
   );
