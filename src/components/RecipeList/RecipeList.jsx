0b85df10a76cc9ce4a1dc3db560ac66652b9dcb6
angeange1 <149048056+angeange1@users.noreply.github.com>
2025-07-06 20:53:41 +0300

    fix of numerous db-queries

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -3,9 +3,15 @@ import LoadMoreBtn from "../LoadMoreBtn/LoadMoreBtn";
 import css from "./RecipeList.module.css";
 import { useDispatch } from "react-redux";
 import { nextPage, nextPageOnSearch } from "../../redux/recipes/slice"
+import { useEffect } from "react";
+import {fetchFavorites} from '../../redux/recipes/operations';
 
 const RecipeList = ({recipes, total, listOnSearch}) => {
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchFavorites());
+  }, [dispatch]);
   
   const handleLoadMore = () => {
     dispatch(nextPage());
