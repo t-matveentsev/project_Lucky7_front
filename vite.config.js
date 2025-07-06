88692c8775f23ba4286d0a3b8e981285ef943e35
BoredDarkness <gwinnblaidd@gmail.com>
2025-07-06 16:10:05 +0200

    Refresh is worging check problems with UI answer

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true,
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://project-lucky7.onrender.com', // http://localhost:3000
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
